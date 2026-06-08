ba5fed1bbcb6030f4eb0349538ec4c0a2c888f23
Noble <noblehallwebsite@gmail.com>
2026-06-08 18:39:21 +0100

    added tokenizer instead of character based chunking

--- backend/app/services/chunking_service.py
+++ backend/app/services/chunking_service.py
@@ -1,14 +1,48 @@
+# def chunk_text(
+#     text: str,
+#     chunk_size: int = 500
+# ):
+
+#     chunks = []
+
+#     for i in range(0, len(text), chunk_size):
+
+#         chunk = text[i:i + chunk_size]
+
+#         chunks.append(chunk)
+
+#     return chunks
+
+
+
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained(
+    "sentence-transformers/all-MiniLM-L6-v2"
+)
+
+
 def chunk_text(
     text: str,
-    chunk_size: int = 500
+    chunk_size: int = 256
 ):
+    # Convert text to tokens
+    tokens = tokenizer.encode(
+        text,
+        add_special_tokens=False
+    )
 
     chunks = []
 
-    for i in range(0, len(text), chunk_size):
+    for i in range(0, len(tokens), chunk_size):
 
-        chunk = text[i:i + chunk_size]
+        chunk_tokens = tokens[i:i + chunk_size]
 
-        chunks.append(chunk)
+        chunk_text = tokenizer.decode(
+            chunk_tokens,
+            skip_special_tokens=True
+        )
+
+        chunks.append(chunk_text)
 
     return chunks
